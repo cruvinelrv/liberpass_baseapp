99939d3b8641b352bb51c806986f04b3321cc3e9
Vinicius Cruvinel <vinicius.cruvinel@ilia.digital>
2023-02-23 18:13:33 -0300

    refactor: base app

---
+++ lib/app/core/application_config.dart
@@ -0,0 +1,12 @@
+class ApplicationConfig {
+  static const String appName = 'My App';
+  static const String appVersion = '1.0.0';
+  static const String appBuildNumber = '1';
+  static const String appFlavor = 'dev';
+  static const String appEnvironment = 'development';
+  static const String appEnvironmentUrl = 'http://localhost:3000';
+
+  void configureApp() {
+    // Configure the app
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,21 @@
-import 'package:flut_micro_commons_dependencies/flut_micro_commons_dependencies.dart';
-import 'package:flut_micro_commons_design_system/flut_micro_commons_design_system.dart';
 import 'package:flutter/material.dart';
+import 'package:liberpass_app/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const LiberPass());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LiberPass extends StatelessWidget {
+  const LiberPass({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'LiberPass',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    Modular.to.navigate('/home');
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            DSTextFormField.currency(
-              keyboardType: TextInputType.number,
-            ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
